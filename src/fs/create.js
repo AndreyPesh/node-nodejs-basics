b8e7191d68e9b93ee1d22b98fce1e1340812ac6f
Andrey Peshko <coursestud@mail.ru>
2022-06-02 16:39:31 +0300

    feat: solve create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,34 @@
+import { access, writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NAME_DIR = 'files';
+const NAME_FILE = 'fresh.txt';
+const pathFile = path.join(__dirname, NAME_DIR, NAME_FILE);
+const CONTENT_FILE = 'I am fresh and young';
+
 export const create = async () => {
-    // Write your code here 
+    try {
+        const isExistsFile = await isCheckExistFile(pathFile);
+        if (isExistsFile) {
+            throw new Error('FS operation failed');
+        }
+        await writeFile(pathFile, CONTENT_FILE);
+    } catch(err) {
+        console.log(err.message);
+    }
+    
 };
+
+const isCheckExistFile = async (pathFile) => {
+    try {
+        await access(pathFile);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+create();
