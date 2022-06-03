454e3459c1b462cc8e80946c6c93727daf71c29b
Andrey Peshko <coursestud@mail.ru>
2022-06-03 14:59:09 +0300

    feat: solve read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,33 @@
+import * as fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NAME_DIR = 'files';
+const NAME_FILE = 'fileToRead.txt';
+const pathToFile = path.join(__dirname, NAME_DIR, NAME_FILE);
+const ERROR_MESSAGE = 'FS operation failed';
+
+const readFile = async (pathToFile) => {
+    try {
+        const fileContent = await fs.readFile(pathToFile,  { encoding: 'utf8' });
+        return fileContent;
+    } catch {
+        return false;
+    }
+}
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        const fileContent = await readFile(pathToFile);
+        if(!fileContent) {
+            throw new Error(ERROR_MESSAGE);
+        }
+        console.log(fileContent);
+    } catch(error) {
+        console.log(error.message);
+    }
 };
+
+read();
