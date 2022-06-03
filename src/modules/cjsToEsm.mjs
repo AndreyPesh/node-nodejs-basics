c1ac504373a4568d0d7f1ced6153fdd9aba50397
Andrey Peshko <coursestud@mail.ru>
2022-06-04 01:22:42 +0300

    feat: solve modules

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,7 +1,13 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path  from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+import { release, version } from 'os';
+import { createServer as createServerHttp }  from 'http';
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
 
 const random = Math.random();
 
@@ -24,7 +30,7 @@ const createMyServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
-module.exports = {
+export {
     unknownObject,
     createMyServer,
 };
