cc9825d84c5d0be8c7c7422b2ac5bb2e20bc88fc
Andrey Peshko <coursestud@mail.ru>
2022-06-03 14:48:51 +0300

    feat: solve list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,34 @@
+import * as fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NAME_DIR = 'files';
+const pathToDir = path.join(__dirname, NAME_DIR);
+const ERROR_MESSAGE = 'FS operation failed';
+
+const readDir = async (pathToDir) => {
+    try {
+        const listFiles = await fs.readdir(pathToDir);
+        return listFiles;
+    } catch {
+        return false;
+    }
+}
+
 export const list = async () => {
-    // Write your code here 
+    try {
+        const folderContent = await readDir(pathToDir);
+        if(!folderContent) {
+            throw new Error(ERROR_MESSAGE);
+        }
+        folderContent.forEach(filename => {
+            console.log(filename);
+        }) 
+    } catch (error) {
+        console.log(error.message);
+    }
 };
+
+list();
