b045fd38fba73c6f8bfeee2d797acdd14345da34
Andrey Peshko <coursestud@mail.ru>
2022-06-03 20:32:47 +0300

    feat: solve args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,11 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const { argv } = process;
+    const TEMPLATE_FLAG_START = '--';
+    argv.forEach((argvValue, index) => {
+        if(argvValue.startsWith(TEMPLATE_FLAG_START)) {
+            console.log(`${argvValue.slice(2)} is ${argv[index + 1]}`);    
+        }
+    });
 };
+
+parseArgs();
