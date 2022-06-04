ebac1d6902ba398e9fdb54ec5dfb3bd634dd7292
Andrey Peshko <coursestud@mail.ru>
2022-06-04 18:58:34 +0300

    feat: solve compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, 'files/fileToCompress.txt');
+const pathToCompressFile = path.join(__dirname, 'files/archive.gz');
+
 export const compress = async () => {
-    // Write your code here 
+    try {
+        const readableStream = createReadStream(pathToFile);
+        const writeableStream = createWriteStream(pathToCompressFile);
+        await pipeline(readableStream, zlib.createGzip(), writeableStream);
+    } catch(error) {
+        console.log(error.message);
+    }
 };
+
+compress();
