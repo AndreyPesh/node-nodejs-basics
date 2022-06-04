212920c9b65f7280d0b7abceb8948fc5cd360894
Andrey Peshko <coursestud@mail.ru>
2022-06-04 18:58:49 +0300

    feat: solve decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, 'files/fileToCompress.txt');
+const pathToCompressFile = path.join(__dirname, 'files/archive.gz');
+
 export const decompress = async () => {
-    // Write your code here 
+    try {
+        const readableStream = createReadStream(pathToCompressFile);
+        const writeableStream = createWriteStream(pathToFile);
+        await pipeline(readableStream, zlib.createUnzip(), writeableStream);
+    } catch(error) {
+        console.log(error.message);
+    }
 };
+
+decompress();
