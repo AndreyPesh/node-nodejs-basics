07e39e6cdf9e079520befe877aff8f6469206a88
Andrey Peshko <coursestud@mail.ru>
2022-06-04 17:48:36 +0300

    feat: solve readable stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,15 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const RELATIVE_PATH_FILE = 'files/fileToRead.txt';
+const pathToFile = path.join(__dirname, RELATIVE_PATH_FILE);
+
 export const read = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(pathToFile);
+    readableStream.pipe(process.stdout);
 };
+
+read();
