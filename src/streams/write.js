83f0240c98ea25c8ee1cadcd388f80d0d6375b52
Andrey Peshko <coursestud@mail.ru>
2022-06-04 17:33:38 +0300

    feat: solve writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,23 @@
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const relativePath = 'files/fileToWrite.txt';
+const pathToFile = path.join(__dirname, relativePath);
+
 export const write = async () => {
-    // Write your code here 
+    const writableStream = createWriteStream(pathToFile);
+    process.stdout.write('To complete the entry type \'stop\':\n');
+    process.stdin.on('data', (chunk) => {
+        const data = chunk.toString();
+        if (data.trim() === 'stop') {
+            process.stdin.destroy();
+            return;
+        }
+        writableStream.write(data);
+    });
 };
+
+write();
