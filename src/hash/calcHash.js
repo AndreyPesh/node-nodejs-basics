c6ac4bf0059e9d0af2a8a7bf18820e5634b29df1
Andrey Peshko <coursestud@mail.ru>
2022-06-04 12:16:05 +0300

    feat: solve task1

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,24 @@
+const { createHash } = await import('crypto');
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NAME_DIR = 'files';
+const NAME_FILE = 'fileToCalculateHashFor.txt';
+const pathToFile = path.join(__dirname, NAME_DIR, NAME_FILE);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const file = await readFile(pathToFile, { encoding: 'utf8' });
+        const hash = createHash('sha256');
+        const hexHashFile = hash.update(file).digest('hex');
+        console.log(hexHashFile);
+        return hexHashFile;
+    } catch (error) {
+        console.log(error.message);
+    }
 };
+
+calculateHash();
